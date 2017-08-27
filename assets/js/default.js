c41f02c31456238a4444e09da5e6095269d74b34
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2017-08-28 00:33:24 +0200

    Add resizeing funtion to header

--- assets/js/default.js
+++ assets/js/default.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
     } else {
         $('.carousel-inner, .carousel-item').height(window.innerHeight+100);
     }
+
+    $(window).resize(function(){
+        if(window.innerWidth > 375){
+            $('.carousel-inner, .carousel-item').height(window.innerHeight-74);
+        } else {
+            $('.carousel-inner, .carousel-item').height(window.innerHeight+100);
+        }
+    });
 });
